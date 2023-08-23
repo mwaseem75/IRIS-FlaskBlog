5cb27aea39d1f388b91eb2a51a733bbbc76c00b3
mwaseem75 <mwaseem75@hotmail.com>
2023-08-23 17:37:12 +0300

    commit

--- src/Python/website/__init__.py
+++ src/Python/website/__init__.py
@@ -2,17 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 from flask_login import LoginManager
-from .myconfig import * 
+from .myconfig import *
 
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "iris-FlaskBlogKey"
-    #Getting DB parameters from myconfig.py file
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'iris://'+DB_USER+':'+DB_PASS+'@'+DB_URL+':'+DB_PORT+'/'+DB_NAMESPACE
-    app.app_context().push()   
-       
+    # Getting DB parameters from myconfig.py file
+    print('iris://'+DB_USER+':'+DB_PASS+'@' +
+          DB_URL+':'+DB_PORT+'/'+DB_NAMESPACE)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'iris://'+DB_USER + \
+        ':'+DB_PASS+'@'+DB_URL+':'+DB_PORT+'/'+DB_NAMESPACE
+    app.app_context().push()
+
     from .views import views
     from .auth import auth
 
@@ -25,7 +29,7 @@ def create_app():
     with app.app_context():
         db.create_all()
 
-    #Assign Login View 
+    # Assign Login View
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
--- src/Python/website/myconfig.py
+++ src/Python/website/myconfig.py
@@ -1,7 +1,7 @@
-#Define config variables
+# Define config variables
 DB_USER = "_SYSTEM"
-DB_PASS = "sys"
+DB_PASS = "SYS"
 DB_URL = "localhost"
-DB_PORT = '52775'
-DB_NAMESPACE="USER"
+DB_PORT = '1972'
+DB_NAMESPACE = "USER"
 PAGINATE_RECORDS_PER_PAG = 3
